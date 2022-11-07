14184b7669565d07e23d8805eb5f898edbe04278
AadiJan <112893464+AadiJan@users.noreply.github.com>
2022-11-07 16:09:59 -0500

    Add files via upload

---
+++ crowdfunding_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "backer" (
+    "backer_id" VARCHAR   NOT NULL,
+    "cf_id" VARCHAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL
+);
+
+
+SELECT * FROM backer;
+
+DROP TABLE backer
