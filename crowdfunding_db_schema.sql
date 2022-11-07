2bd26b68c7310146cca78a7feec389896168dfe0
AadiJan <112893464+AadiJan@users.noreply.github.com>
2022-11-07 15:06:38 -0500

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,12 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "backer" (
+    "backer_id" int   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar()   NOT NULL,
+    "last_name" varchar()   NOT NULL,
+    "email" varchar()   NOT NULL
+);
+
